6383e03ddb144b6dcc9039ac930a516e4685ac82
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-03-07 19:04:31 +0000

    complete homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+// import Demo from './components/Login'
+import Home from './components/Home'
+// import NotFound from './components/NotFound'
+import { Routes, Route } from "react-router-dom"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/demo" element={<Demo />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+  )
 }
 
 export default App;
+
